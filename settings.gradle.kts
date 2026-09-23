0fc6c5ca785246340726cef4571dc6b86b4ae9e4
ryzen-admin <serge.loie@yandex.ru>
2026-09-23 15:22:26 +0500

    modernize gradle build config

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,15 @@
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    repositories {
+        mavenCentral()
+    }
+}
+
 rootProject.name = "jmkvpropedit"
